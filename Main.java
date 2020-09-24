ab26997cd903bc58216ac8e004bd9d27cb926aa0
zamekouar06 <zackaria.mekouar@patelhs.org>
2020-09-24 12:32:40 +0000

    the <> was added

--- Main.java
+++ Main.java
@@ -1,5 +1,18 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int myAge = 14;
+    int bDay = 2006;
+    int thisYear = 2020;
+
+   System.out.println( myAge < thisYear );
+     System.out.println( myAge > thisYear );
+     System.out.println( myAge <= thisYear+1 );
+     System.out.println( myAge-1 >= thisYear );
+     System.out.println(thisYear < bDay); 
+
+  System.out.println("I am" + myAge + "years old");
+
+
+
   }
 }
